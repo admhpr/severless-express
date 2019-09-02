c720114dbea9212abceb4562cbea92173d3bf898
adam <adam@harpur.io>
2019-09-02 21:35:23 +0100

    chore: hello world example

--- src/api/commands.js
+++ src/api/commands.js
@@ -1,44 +1,31 @@
-require('dotenv').config();
-const request = require("request")
+require("dotenv").config();
+const request = require("request");
 
 const commands = require("express").Router();
 
-commands.get('/', (req, res) => {
-    res.json({"commands": "ok"})
-})
+commands.get("/", (req, res) => {
+  res.json({ commands: "ok" });
+});
 
-commands.post('/baptime', (req, res) => {
-    const d = new Date()
-    const data = {
-        form: {
-            token: process.env.SLACK_AUTH_TOKEN,
-            channel: "#test",
-            text: `baps are at ${new Date(d.setDate(d.getDate() + (12 - d.getDay()) % 7)).toLocaleString().slice(0,9)} 9:30am  :thumbsup:`
-        }
+commands.post("/hello", (req, res) => {
+  const d = new Date();
+  const data = {
+    form: {
+      token: process.env.SLACK_AUTH_TOKEN,
+      channel: "#test",
+      text: `hello world`
     }
-    request.post('https://slack.com/api/chat.postMessage', data, (error, res, body) => {
-        if(error){
-            throw new Error(error)
-        }
-    });
-   res.end()
-})
-
-commands.post('/bapgetter', (req, res) => {
-    const d = new Date()
-    const data = {
-        form: {
-            token: process.env.SLACK_AUTH_TOKEN,
-            channel: "#test",
-            text: `:thumbsup:`
-        }
+  };
+  request.post(
+    "https://slack.com/api/chat.postMessage",
+    data,
+    (error, res, body) => {
+      if (error) {
+        throw new Error(error);
+      }
     }
-    request.post('https://slack.com/api/chat.postMessage', data, (error, res, body) => {
-        if(error){
-            throw new Error(error)
-        }
-    });
-   res.end()
-})
+  );
+  res.end();
+});
 
-module.exports = commands
+module.exports = commands;
